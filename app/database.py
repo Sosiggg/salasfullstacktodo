31fd20ab92e1f228fcb5ab8293a3f20b0eeb4ba1
Sosiggg <salas.ivisusej@gmail.com>
2025-04-25 03:47:12 +0800

    Fixed database

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
 DATABASE_URL = "postgresql://salastodolist_fastapi_user:TAVWZx6YBTdnFyxRhL0t7wAXuQPOkTq7@dpg-cvtsip7gi27c73aa7j90-a.singapore-postgres.render.com/salastodolist_fastapi"
-SQLALCHEMY_DATABASE_URL = "postgresql://salastodolist_fastapi_user:TAVWZx6YBTdnFyxRhL0t7wAXuQPOkTq7@dpg-cvtsip7gi27c73aa7j90-a.singapore-postgres.render.com/salastodolist_fastapi"
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 
@@ -17,4 +17,14 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def test_db_connection():
+    try:
+        db = SessionLocal()
+        result = db.execute("SELECT 1")
+        print("Database connection test successful:", result.fetchall())
+        db.close()
+    except SQLAlchemyError as e:
+        print("Error connecting to the database:", e)
+        db.rollback()
 
